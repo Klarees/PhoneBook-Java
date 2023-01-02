2e079879f8e43dfc6ce876dd8cb63643e0707b32
Klarees <dizonclariskimmy@gmail.com>
2023-01-02 14:29:26 +0800

    added if else statement for the list command

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -43,4 +43,20 @@ public class PhoneBook {
             System.err.println("Could not load contacts, phone book is empty!");
         }
     }
+    private static void listContacts(Map<String, List<String>> contacts) {
+        if (!contacts.isEmpty()) {
+            for (Map.Entry<String, List<String>> entry : contacts.entrySet()) {
+                System.out.println(entry.getKey());
+                for (String number : entry.getValue()) {
+                    System.out.println(number);
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("No records found, the phone book is empty!");
+        }
+
+        System.out.println();
+        System.out.println("Type a command or 'exit' to quit. For a list of valid commands use 'help':");
+    }
 }
