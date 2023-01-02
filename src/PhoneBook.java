0b00470a35af28fb5d491727fb84f97698adbc64
Klarees <dizonclariskimmy@gmail.com>
2023-01-02 14:41:11 +0800

    added delete command

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -223,4 +223,35 @@ public class PhoneBook {
         System.out.println();
         System.out.println("Type a command or 'exit' to quit. For a list of valid commands use 'help':");
     }
+    private static void deleteContact(Map<String, List<String>> contacts, Scanner input) {
+        System.out.println("Enter name of the contact to be deleted:");
+        String name = input.nextLine().trim();
+
+        if (contacts.containsKey(name)) {
+            System.out.printf("Contact '%s' will be deleted. Are you sure? [Y/N]:\n", name);
+            String confirmation = input.nextLine().trim().toLowerCase();
+            confirm:
+            while (true) {
+                switch (confirmation) {
+                    case "y":
+                        contacts.remove(name);
+                        saveContacts(contacts);
+                        System.out.println("Contact was deleted successfully!");
+                        break confirm;
+                    case "n":
+                        break confirm;
+                    default:
+                        System.out.println("Delete contact? [Y/N]:");
+                        break;
+                }
+                confirmation = input.nextLine().trim().toLowerCase();
+            }
+
+        } else {
+            System.out.println("Sorry, name not found!");
+        }
+
+        System.out.println();
+        System.out.println("Type a command or 'exit' to quit. For a list of valid commands use 'help':");
+    }
 }
