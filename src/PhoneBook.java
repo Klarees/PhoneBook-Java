f36e0e6aaa7fb95f10b0bb6d456ef7c2e0f4ead1
Klarees <dizonclariskimmy@gmail.com>
2023-01-02 14:32:06 +0800

    added function for finf command

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -75,5 +75,24 @@ public class PhoneBook {
         System.out.println();
         System.out.println("Type a command or 'exit' to quit. For a list of valid commands use 'help':");
     }
+    private static void findContact(Map<String, List<String>> contacts, Scanner input) {
+        System.out.println("Enter a number to see to whom does it belong:");
+        String number = input.nextLine().trim();
 
+        while (!number.matches("^\\+?[0-9 ]{3,25}$")) {
+            System.out.println("Invalid number! May contain only digits, spaces and '+'. Min length 3, max length 25.");
+            System.out.println("Enter number:");
+            number = input.nextLine().trim();
+        }
+
+        for (Map.Entry<String, List<String>> entry : contacts.entrySet()) {
+            if (entry.getValue().contains(number)) {
+                System.out.println(entry.getKey());
+                System.out.println(number);
+            }
+        }
+
+        System.out.println();
+        System.out.println("Type a command or 'exit' to quit. For a list of valid commands use 'help':");
+    }
 }
