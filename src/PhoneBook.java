452eb23fad4fc1888b93fe6a66e36fe576e8d47d
Klarees <dizonclariskimmy@gmail.com>
2023-01-02 14:34:32 +0800

    added add command

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -61,7 +61,7 @@ public class PhoneBook {
         System.out.println();
         System.out.println("Type a command or 'exit' to quit. For a list of valid commands use 'help':");
     }
-    
+
     private static void showContact(Map<String, List<String>> contacts, Scanner input) {
         System.out.println("Enter the name you are looking for:");
         String name = input.nextLine().trim();
@@ -98,5 +98,31 @@ public class PhoneBook {
 
         System.out.println();
         System.out.println("Type a command or 'exit' to quit. For a list of valid commands use 'help':");
+    }
+
+    private static void addContact(Map<String, List<String>> contacts, Scanner input) {
+        System.out.println("You are about to add a new contact to the phone book.");
+        String name;
+        String number;
+
+        while (true) {
+            System.out.println("Enter contact name:");
+            name = input.nextLine().trim();
+            if (name.matches("^.{2,50}$")) {
+                break;
+            } else {
+                System.out.println("Name must be in range 2 - 50 symbols.");
+            }
+        }
+
+        while (true) {
+            System.out.println("Enter contact number:");
+            number = input.nextLine().trim();
+            if (number.matches("^\\+?[0-9 ]{3,25}$")) {
+                break;
+            } else {
+                System.out.println("Number may contain only '+', spaces and digits. Min length 3, max length 25.");
+            }
+        }
     }
 }
